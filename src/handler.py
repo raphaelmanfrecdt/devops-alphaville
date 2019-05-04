9f5fe742c5f8d517cbcbbecc6fe73d3bd8c7e9e4
raphaelmanfrecdt <raphael.manfre.ext@conductor.com.br>
2019-05-04 17:23:52 -0300

    alterações gerais

--- src/handler.py
+++ src/handler.py
@@ -4,7 +4,8 @@ import re
 
 
 def validate_url(input):
-    URL_REGEX = re.compile(r"https?://.*/[a-zA-Z0-9-_,/.]*$", re.UNICODE | re.I)    if re.match(URL_REGEX, input):
+    URL_REGEX = re.compile(r"https?://.*/[a-zA-Z0-9-_,/.]*$", re.UNICODE | re.I)    
+    if re.match(URL_REGEX, input):
         return True
     else:
         return False
